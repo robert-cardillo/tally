9e3a6877141d63a43c889c92be8fa942dc18d83b
Snow Blind <snowy.tmp@gmail.com>
2014-08-04 20:30:48 +0300

    rollback: login input type number/password

--- login.php
+++ login.php
@@ -3,10 +3,8 @@ include('header.php');
 ?>
 <div style="width: 100vw; height: 100vh; text-align: center; display: table-cell; vertical-align: middle">
     <form action="" method="post">
-        <input type="number" style="width: 6ex;" name="pin"
-               value="" <?= isset($login_error) ? ('style="border-color: red;"') : ('') ?>
-               onfocus="this.type='password';this.focus()" onblur="this.type='number'">
-    <input type="submit" name="login" value="Giriş">
+        <input type="password" size="5" name="pin" value=""<?=isset($login_error)?(' style="border-color: red;"'):('')?>>
+        <input type="submit" name="login" value="Giriş">
     </form>
 </div>
 <?
